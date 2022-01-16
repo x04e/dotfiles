85879a474f83b3dc7d3a992bf6c0c39d451fa8ea
Liam Hilliard <liam.h.-@hotmail.com>
2022-01-16 15:39:40 +0000

    Update shell prompt
    Implemented custom git information, changed colours, and added newline

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,52 +1,20 @@
 #!/bin/zsh
-__fastgit_ps1 () {
-    local headfile head branch
-    local dir="$PWD"
-
-    while [ -n "$dir" ]; do
-        if [ -e "$dir/.git/HEAD" ]; then
-            headfile="$dir/.git/HEAD"
-            break
-        fi
-        dir="${dir%/*}"
-    done
-
-    if [ -e "$headfile" ]; then
-        read -r head < "$headfile" || return
-        case "$head" in
-            ref:*) branch="${head##*/}" ;;
-            "") branch="" ;;
-            *) branch="${head:0:7}" ;;  #Detached head. You can change the format for this too. 
-        esac
-    fi
-
-    if [ -z "$branch" ]; then
-        return 0
-    fi
-
-    if [ -z "$1" ]; then
-        # Default format
-        printf "(%s) " "$branch"
-    else
-        # Use passed format string
-        printf "$1" "$branch"
-    fi
-}
+# PS1 with git status built in
 
 _prompt () {
+    exec 2> /dev/null
     local bg="234"
-    local fg1="242"
-    local fg2="247"
-    local fg3="006"
-    local fg4="242"
-    local pl="\ue0b0"
+    local fg1="008"
+    local fg2="011"
+    local fg3="012"
+    local fg4="008"
+    local BRANCH="$(git branch --show-current)"
+    local CHANGES="$(git status --porcelain | wc -l | sed 's/^/ /g; s/^ 0$//g')"
 
-    local gitbranch="$(__fastgit_ps1 '%s')"
-
-    [[ $gitbranch ]] && {
-        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%~ %F{$fg3}$gitbranch%F{$fg4}%K{$bg} >%F{$bg}%k$pl%f "
+    [ "$BRANCH" ] && {
+        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%d %F{$fg3}($BRANCH)$CHANGES%F{$fg4}%K{$bg}%F{$bg}%k\ue0b0%f\n%K{$bg}%F{$fg4} >%K{$bg}%F{$bg}%k\ue0b0%f "
     } || {
-        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%~ %F{$fg4}>%F{$bg}%k$pl%f "
+        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%d %F{$fg4}%F{$bg}%k\ue0b0%f\n%K{$bg}%F{$fg4} >%K{$bg}%F{$bg}%k\ue0b0%f "
     }
 }
 
