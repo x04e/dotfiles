59a3a0a201f0ef8beb7480ceec05de3c7e18cf3c
Liam Hilliard <liam.h.-@hotmail.com>
2022-04-29 23:27:06 +0100

    Update prompt with git unstaged/staged file info

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -9,9 +9,15 @@ _prompt () {
     local fg3="012"
     local fg4="008"
     local BRANCH="$(git branch --show-current)"
+    local CHANGES=""
+
+    # Note: If this runs outside a Git repository, the exit code will be 129, so CHANGES will be "*^". Therefore don't use CHANGES in conditionals
+    git diff --no-ext-diff --quiet || CHANGES+="*" # unstaged changes
+    git diff --no-ext-diff --cached --quiet || CHANGES+="^" # staged changes
+    [ "$CHANGES" ] && CHANGES=" $CHANGES" # Add leading space if changes isn't empty
 
     [ "$BRANCH" ] && {
-        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%d %F{$fg3}($BRANCH)$CHANGES%F{$fg4}%K{$bg}%F{$bg}%k\ue0b0%f\n%K{$bg}%F{$fg4} >%K{$bg}%F{$bg}%k\ue0b0%f "
+        echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%d %F{$fg3}($BRANCH$CHANGES)%F{$fg4}%K{$bg}%F{$bg}%k\ue0b0%f\n%K{$bg}%F{$fg4} >%K{$bg}%F{$bg}%k\ue0b0%f "
     } || {
         echo "%K{$bg}%F{$fg1} %D{%H:%M:%S} %F{$fg2}%d %F{$fg4}%F{$bg}%k\ue0b0%f\n%K{$bg}%F{$fg4} >%K{$bg}%F{$bg}%k\ue0b0%f "
     }
